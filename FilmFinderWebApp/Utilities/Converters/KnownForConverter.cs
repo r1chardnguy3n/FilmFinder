cf010c7e70a334eab24df8cecff36151fbb37eee
Richard Nguyen <reechardc@gmail.com>
2020-03-03 20:06:23 -0800

    Added files from TMBDLib
    added filed from TMBDLib github repo by Lord Mike 
    https://github.com/LordMike/TMDbLib

---
+++ FilmFinderWebApp/Utilities/Converters/KnownForConverter.cs
@@ -0,0 +1,28 @@
+using System;
+using Newtonsoft.Json.Linq;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Search;
+
+namespace TMDbLib.Utilities.Converters
+{
+    internal class KnownForConverter : JsonCreationConverter<KnownForBase>
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(KnownForBase);
+        }
+
+        protected override KnownForBase GetInstance(JObject jObject)
+        {
+            MediaType mediaType = jObject["media_type"].ToObject<MediaType>();
+
+            switch (mediaType)
+            {
+                case MediaType.Movie:
+                    return new KnownForMovie();
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
